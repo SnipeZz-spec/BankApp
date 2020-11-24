a289661fecdf597436f2dc1841b40c150f421c44
SnipeZz-spec <goprosnipezz@yandex.ru>
2020-11-24 20:41:21 +0300

    1

--- Cards/MaestroCard.java
+++ Cards/MaestroCard.java
@@ -3,6 +3,7 @@ package Cards;
 public class MaestroCard extends GeneralCard {
 
 
+
     public MaestroCard(String name, int number, int month, int year, double balance) {
         super(name, number, month, year, balance);
     }
